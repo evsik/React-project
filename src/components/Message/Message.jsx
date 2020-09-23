4a5335c48dc9e80dd4f261c755881f5e829ec31a
evsik <evsevsikov@gmail.com>
2020-09-23 14:26:58 +0300

    some changes

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -7,7 +7,7 @@ export default props => {
     return (
 
         <div className="d-flex flex-column msg" style={{ alignSelf: props.sender !== "Darth Vader" ?
-                'flex-start' : 'flex-end' }}>
+                'flex-end' : 'flex-start' }}>
             {sender && <strong>{sender}:</strong>}
             {!sender && <strong>You:</strong>}
             <p>{sender || (!sender && text) ? text : 'Cyber answer...'}</p>
